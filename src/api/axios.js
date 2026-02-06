16ca26bbd7d9b70ff0718b721cb87c067792f56d
fo-xxy <robert.yarce.12@hotmail.com>
2026-02-06 18:27:29 -0500

    Modificación del api, para que funcionara correctamente con la actualización del api, ya que se implementó con docker

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost/Mini_Gestor_Tareas/api/', 
+    baseURL: 'http://localhost:8080/api/', 
     headers: {
         'Content-Type': 'application/json'
     }
