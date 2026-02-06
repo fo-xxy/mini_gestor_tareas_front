2c84b7615fe764222f42be4ec0c95c73e70059a3
fo-xxy <robert.yarce.12@hotmail.com>
2026-02-06 13:40:28 -0500

    Actualización de readme con las intrucciones para ejecutarlo

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,6 @@ import { useSelector } from 'react-redux';
 import Auth from './pages/Auth';
 import Tasks from './pages/Tasks';
 
-const TasksPlaceholder = () => (
-  <div className="p-10 text-center">
-    <h1 className="text-2xl font-bold">¡Bienvenido al Gestor de Tareas!</h1>
-    <p>Aquí cargaremos la lista de tu base de datos de Phalcon.</p>
-  </div>
-);
-
 function App() {
 
   const { token } = useSelector((state) => state.auth);
